b0fb7a2800e7d820285c2afd46b6eadc0d8f43de
Marie Jo Angela Misa <misa@stormbenefits.com>
2019-10-14 03:07:36 +0800

    User Model: validate user email

--- app/core/models.py
+++ app/core/models.py
@@ -11,6 +11,9 @@ class UserManager(BaseUserManager):
         """
         # Normalizes email addresses by lowercasing the domain
         # portion of the email address.
+        if not email:
+            raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
