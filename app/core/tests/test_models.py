539536da45067281c616b7d28416cbd248ad5194
Marie Jo Angela Misa <misa@stormbenefits.com>
2019-10-14 03:03:22 +0800

    User Model: Normalize user email format.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -17,3 +18,15 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
+    def test_new_user_email_normalized(self):
+        """
+        Test the email for a new user is normalized (char cases).
+        """
+        email = 'test@LONDONAPPDEV.COM'
+        password = "Password123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
